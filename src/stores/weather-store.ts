357f526c0aba76d323ca1ce7ae6a79d034a96fe3
Andrew McKeever <andrewmckeever@Andrews-MBP.fritz.box>
2025-04-18 21:49:38 +0200

    create state store

---
+++ src/stores/weather-store.ts
@@ -0,0 +1,16 @@
+import { Current, DayDetails, Forecast } from "@/types/weather";
+import { create } from "zustand";
+
+interface WeatherStore {
+  forecast: Forecast[];
+  current: Current | null;
+  setForecast: (data: Forecast[]) => void;
+  setCurrent: (data: Current) => void;
+}
+
+export const useWeatherStore = create<WeatherStore>()((set) => ({
+  forecast: [],
+  current: null,
+  setForecast: (data: Forecast[]) => set({ forecast: data }),
+  setCurrent: (data: Current) => set({ current: data })
+}));
