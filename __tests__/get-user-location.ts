e546b6aaf2a0c66a037c0a8502a3211bc5e51760
Andrew McKeever <andrewmckeever@Andrews-MBP.fritz.box>
2025-04-18 21:46:10 +0200

    setup unit tests for user location and weather data

---
+++ __tests__/get-user-location.ts
@@ -0,0 +1,33 @@
+import { getUserLocationCoordinates } from "@/lib/get-user-location";
+import { WebServiceClient } from "@maxmind/geoip2-node";
+
+describe("getUserLocationCoordinates", () => {
+  const mockIp = "192.168.1.1";
+
+  it("should return coordinates from client.city if available", async () => {
+    const result = await getUserLocationCoordinates(mockIp);
+
+    expect(result).toEqual({
+      latitude: -33.3013,
+      longitude: 26.5325
+    });
+  });
+
+  it("should return default coordinates if location is undefined", async () => {
+    const result = await getUserLocationCoordinates(mockIp);
+
+    expect(result).toEqual({
+      latitude: -33.3013,
+      longitude: 26.5325
+    });
+  });
+
+  it("should return default coordinates if client.city throws an error", async () => {
+    const result = await getUserLocationCoordinates(mockIp);
+
+    expect(result).toEqual({
+      latitude: -33.3013,
+      longitude: 26.5325
+    });
+  });
+});
