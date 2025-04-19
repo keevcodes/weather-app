d9cf58a816bfaf2257e88889fecda824ac55e00c
Andrew McKeever <andrewmckeever@Andrews-MBP.fritz.box>
2025-04-19 09:11:27 +0200

    add an animation to the weather list items

--- src/components/forecast-list.tsx
+++ src/components/forecast-list.tsx
@@ -1,29 +1,46 @@
 "use client";
 
 import { useWeatherStore } from "@/stores/weather-store";
-import { Current } from "@/types/weather";
+import { SelectedDay } from "@/types/weather";
 import dayjs from "dayjs";
+import { Calendar } from "lucide-react";
 import Image from "next/image";
 
 export const ForecastList = () => {
   const forecast = useWeatherStore((state) => state.forecast);
-  const setCurrent = useWeatherStore((state) => state.setCurrent);
+  const current = useWeatherStore((state) => state.current);
+  const setSelectedDayForOverview = useWeatherStore(
+    (state) => state.setSelectedDayForOverview
+  );
 
-  const handleSeeWeatherDetails = (day: Current) => {
-    setCurrent(day);
+  const handleSeeWeatherDetails = (day: SelectedDay) => {
+    if (current && day.date === dayjs().format("YYYY-MM-DD")) {
+      setSelectedDayForOverview({
+        ...current,
+        maxtemp_c: day.maxtemp_c,
+        mintemp_c: day.mintemp_c,
+        date: day.date
+      });
+      return;
+    }
+
+    setSelectedDayForOverview(day);
   };
-
-  console.log({ forecast });
 
   return (
     <div className="w-full bg-slate-500/75 rounded-lg p-4 shadow-lg">
+      <div className="flex items-center border-b-1 border-white/50 pb-2">
+        <Calendar className="text-white/50 w-4 mr-2" />
+        <h4 className="text-white/50 font-inter text-sm">7 Day Forecast</h4>
+      </div>
       <ul className="flex flex-col w-full">
         {forecast.map(({ day, date }, index) => (
           <li
             key={date}
             className={` p-4 text-white ${
-              index < forecast.length - 1 ? "border-b border-white" : ""
-            }`}
+              index < forecast.length - 1 ? "border-b border-white/50" : ""
+            }
+            transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-md`}
           >
             <button
               className="w-full flex justify-between items-center cursor-pointer"
@@ -31,7 +48,9 @@ export const ForecastList = () => {
             >
               <div className="flex flex-col">
                 <span className="text-md font-inter">
-                  {dayjs(date).format("dddd")}
+                  {date === dayjs().format("YYYY-MM-DD")
+                    ? "Today"
+                    : dayjs(date).format("dddd")}
                 </span>
                 <span className="text-xs text-muted-foreground font-inter text-white/40">
                   {dayjs(date).format("DD/MM/YYYY")}
